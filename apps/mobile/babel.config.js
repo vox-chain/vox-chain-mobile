c0e4f5c46504161b34f6d49b983a315d5fc81d16
Youssef Maghzaz <youssefmaghzaz@gmail.com>
2024-10-28 13:25:27 +0100

    remove crypto

--- apps/mobile/babel.config.js
+++ apps/mobile/babel.config.js
@@ -2,17 +2,6 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: [['babel-preset-expo', { jsxImportSource: 'nativewind' }], 'nativewind/babel'],
-    plugins: [
-      [
-        'module-resolver',
-        {
-          alias: {
-            crypto: 'react-native-quick-crypto',
-            stream: 'readable-stream',
-            buffer: '@craftzdog/react-native-buffer',
-          },
-        },
-      ],
-    ],
+    plugins: ['module-resolver'],
   };
 };
--- apps/mobile/polyfills.js
+++ apps/mobile/polyfills.js
@@ -2,9 +2,9 @@ import 'react-native-gesture-handler';
 
 import '@formatjs/intl-pluralrules/polyfill-force';
 
-import { Platform } from 'react-native';
+// import { Platform } from 'react-native';
 
-if (Platform.OS !== 'web') {
-  const { install } = require('react-native-quick-crypto');
-  install();
-}
+// if (Platform.OS !== 'web') {
+//   const { install } = require('react-native-quick-crypto');
+//   install();
+// }
