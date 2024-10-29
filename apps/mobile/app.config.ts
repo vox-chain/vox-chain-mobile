fe69897c8de0e87b2e96f595236a4a17ba19aa72
Youssef Maghzaz <youssefmaghzaz@gmail.com>
2024-10-29 15:40:54 +0100

    remove firebase dep

--- apps/mobile/app.config.ts
+++ apps/mobile/app.config.ts
@@ -1,28 +1,23 @@
 import { ConfigContext, ExpoConfig } from 'expo/config';
 
-export default ({ config }: ConfigContext): ExpoConfig => ({
-  ...config,
-  name: config.name || 'FT Hackathon 2024',
-  slug: config.slug || 'ft-hackathon-2024',
-  owner: 'fevertokens',
-  android: {
-    ...config.android,
-    googleServicesFile:
-      process.env.GOOGLE_SERVICES_JSON ||
-      config.android?.googleServicesFile ||
-      './google-services.json',
-  },
-  ios: {
-    ...config.ios,
-    googleServicesFile:
-      process.env.GOOGLE_SERVICES_PLIST ||
-      config.ios?.googleServicesFile ||
-      './GoogleService-Info.plist',
-  },
-  updates: {
-    url: 'https://u.expo.dev/a57c3d36-d29b-40ed-a2f3-4c9cb7ee0204',
-  },
-  runtimeVersion: {
-    policy: 'appVersion',
-  },
-});
+export default ({ config }: ConfigContext): ExpoConfig => {
+  console.log('config =', config);
+  return {
+    ...config,
+    name: config.name || 'FT Hackathon 2024',
+    slug: config.slug || 'ft-hackathon-2024',
+    owner: 'fevertokens',
+    android: {
+      ...config.android,
+    },
+    ios: {
+      ...config.ios,
+    },
+    updates: {
+      url: 'https://u.expo.dev/a57c3d36-d29b-40ed-a2f3-4c9cb7ee0204',
+    },
+    runtimeVersion: {
+      policy: 'appVersion',
+    },
+  };
+};
